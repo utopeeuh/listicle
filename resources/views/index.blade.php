340e67bc7e2b5fa953d303c471919b4686764061
Uto <daffamadeo@gmail.com>
2022-01-03 17:49:51 +0700

    improved button ui/ux

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,11 +7,13 @@
     <div class="container p-5">
         @if ($notes->count() > 0)
             <div class="row justify-content-center align-items-center mb-4">
-                <div class="col-md-6">
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#create-modal">
+                <div class="col-md-5">
+                    <button type="button" class="btn btn-text btn-green-text" data-bs-toggle="modal"
+                        data-bs-target="#create-modal">
                         Add new task
                     </button>
-                    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#clear-modal">
+                    <button type="button" class="btn btn-text btn-red-text" data-bs-toggle="modal"
+                        data-bs-target="#clear-modal">
                         Clear all
                     </button>
                 </div>
@@ -20,19 +22,21 @@
 
         @forelse ($notes as $note)
             <div class="row justify-content-center align-items-center mb-1">
-                <div for="task-{{ $note->id }}-cb" class="task task-{{ $note->priority }} col-md-4 align-items-center">
-                    <input type="checkbox" id="task-{{ $note->id }}-cb" onclick="isdone({{ $note->id }});"
-                        {{ $note->isdone == 1 ? 'checked' : '' }} />
-                    <span class="st"> {{ $note->title }}</span>
+                <div class="col-md-4 align-items-center task-cb">
+                    <input type="checkbox" id="task-{{ $note->id }}-cb" name="task-{{ $note->id }}-cb"
+                        onclick="isdone({{ $note->id }});" {{ $note->isdone == 1 ? 'checked' : '' }} />
+                    <label class="cb-check" for="task-{{ $note->id }}-cb"></label>
+                    <label for="task-{{ $note->id }}-cb" class="task task-{{ $note->priority }}">
+                        {{ $note->title }}</label>
                 </div>
-                <div class="col-md-2">
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                <div class="col-md-auto">
+                    <button type="button" class="btn btn-text btn-green-text" data-bs-toggle="modal"
                         data-bs-target="#update-modal-{{ $note->id }}">
-                        Edit
+                        <i class="far fa-edit"></i>
                     </button>
-                    <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                    <button type="button" class="btn btn-text btn-red-text" data-bs-toggle="modal"
                         data-bs-target="#delete-modal-{{ $note->id }}">
-                        Delete
+                        <i class="far fa-trash-alt"></i>
                     </button>
                 </div>
             </div>
@@ -45,8 +49,8 @@
                     No tasks found, start by<br>clicking the button below
                 </div>
 
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#create-modal">
-                    Add new task
+                <button type="button" class="btn btn-plus" data-bs-toggle="modal" data-bs-target="#create-modal">
+                    <i class="fas fa-plus"></i>
                 </button>
 
             </div>
